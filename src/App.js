870441aeed67336375fccaa45cbae7760953a32e
cem2411 <cem.gueclue@hotmail.de>
2019-11-14 19:45:35 +0100

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import "./App.css";
+import React, {Component} from "react";
 
-function App() {
+
+class App extends Component{
+  render(){
   return (
     <div className="App">
       <h1>Test</h1>
     </div>
   );
+  }
 }
 
 export default App;
