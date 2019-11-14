572636bdacfcd980d214610c2afd45a32ee97bb5
cem2411 <cem.gueclue@hotmail.de>
2019-11-14 23:39:17 +0100

    routing-testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, {Component} from "react";
+import Recact from 'react'
+import React, {Component} from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Navbar} from './components/navbar/Navbar'
 
+import Button from 'react-bootstrap/Button'
 
 class App extends Component{
   render(){
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <BrowserRouter>
+    <div className="container">
+    <Switch>
+      <Route path='/navbar' component={Navbar}/>
+
+    </Switch>
     </div>
-  );
+    </BrowserRouter>
+    );
   }
 }
 
