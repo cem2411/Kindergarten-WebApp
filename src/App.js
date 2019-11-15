b484ecb071adf85132f77aecaf1e60c3b72746e1
cem2411 <cem.gueclue@hotmail.de>
2019-11-15 02:26:04 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Recact from 'react'
 import React, {Component} from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import {Navbar} from './components/navbar/Navbar'
+import {Navbar, Navigation} from './components/navbar/Navigation'
 import {Eattendance} from './components/submenu/educator/Eattendance'
 import {Pattendance} from './components/submenu/parent/Pattendance'
 
@@ -12,10 +12,12 @@ import { format } from 'path'
 class App extends Component{
   render(){
   return (
+    
     <BrowserRouter>
     <div className="container">
-    <h1>Home</h1>
+    
     <Switch>
+      <Route path='/' component= {Navigation}/>
       <Route path='/Eattendance' component= {Eattendance}/>
       <Route path='/Pattendance' component= {Pattendance}/>
       </Switch>
---
+++ src/components/navbar/Navigation.js
@@ -0,0 +1,27 @@
+ import React, { Component } from 'react'
+ import {Nav, Navbar} from 'react-bootstrap';
+
+  export  class Navigation extends Component {
+     render() {
+         return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="/">kiga2go</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              
+              <Nav className="mr-auto">
+                <Nav.Link href="/Eattendance">Eattendance</Nav.Link>
+                <Nav.Link href="/Pattendance">Pattendance</Nav.Link>                
+              </Nav>
+
+              <Nav>
+                <Nav.Link href="#deets">PROFIL</Nav.Link>      
+              </Nav>
+
+            </Navbar.Collapse>
+          </Navbar>
+         )
+     }
+ }
+ 
+ 
