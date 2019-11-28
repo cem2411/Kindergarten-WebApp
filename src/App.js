c55fc1fcdf68983ee9acce6058a0f1b0788ea17b
cem2411 <cem.gueclue@hotmail.de>
2019-11-28 17:04:32 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import "./style.scss";
+import background from "./img/background/background.jpg";
+
 import { Navigation } from "./components/navbar/Navigation";
 import { Pattendance } from "./pages/parent-view/absence/Pattendance";
 import { Info } from "./pages/common-view/info/Info";
 import { Footer } from "./components/footer/Footer";
 import { LoginForm } from "./components/forms/login/LoginForm";
-
-import "./style.scss";
 import RegisterForm from "./components/forms/register/RegisterForm";
 import { ListChildren } from "./pages/admin-view/listChildren/ListChildren";
 
@@ -19,13 +21,15 @@ class App extends Component {
             <Navigation />
           </header>
 
-          <Switch>
-            <Route path="/Pattendance" component={Pattendance} />
-            <Route path="/Info" component={Info} />
-            <Route path="/Login" component={LoginForm} />
-            <Route path="/AddChild" component={RegisterForm} />
-            <Route path="/ListChild" component={ListChildren} />
-          </Switch>
+          <div className="page-content">
+            <Switch>
+              <Route path="/Pattendance" component={Pattendance} />
+              <Route path="/Info" component={Info} />
+              <Route path="/Login" component={LoginForm} />
+              <Route path="/AddChild" component={RegisterForm} />
+              <Route path="/ListChild" component={ListChildren} />
+            </Switch>
+          </div>
 
           <footer>
             <Footer />
