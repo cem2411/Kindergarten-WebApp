f33df6c941e566170c151a03e65352fcea73ce53
cem2411 <cem.gueclue@hotmail.de>
2019-12-20 10:51:46 +0100

    login-form

--- kiga2go-react/src/App.js
+++ kiga2go-react/src/App.js
@@ -1,22 +1,28 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import "./style.scss";
 import background from "./img/background/background.jpg";
-
 import { Navigation } from "./components/navbar/Navigation";
 import { Pabsence } from "./pages/parent-view/absence/Pabsence";
 import { Info } from "./pages/common-view/info/Info";
 import { Footer } from "./components/footer/Footer";
-import { LoginForm } from "./components/forms/login/LoginForm";
+import LoginForm from "./components/forms/login/LoginForm";
 import RegisterForm from "./components/forms/register/RegisterForm";
 import { ListChildren } from "./pages/admin-view/listChildren/ListChildren";
 import { Aabsence } from "./pages/admin-view/listAbsence/Aabsence";
 import AbsenceForm from "./components/forms/absence/AbsenceForm";
+import { UserProvider } from "./context/user-context";
+import "./style.scss";
 
-class App extends Component {
-  render() {
-    return (
+export default function App() {
+  const [user, setUser] = useState();
+
+  const validateUser = ({ email, password }) => {
+    console.log(email);
+    console.log(password);
+  };
+
+  return (
+    <UserProvider value={user}>
       <BrowserRouter>
         <div className="main-container">
           <header>
@@ -24,23 +30,23 @@ class App extends Component {
           </header>
 
           <div className="page-content">
-            <Switch>
-              <Route path="/Login" component={LoginForm} />
-              <Route path="/ListChildren" component={ListChildren} />
-              <Route path="/Info" component={Info} />
-              <Route path="/Absence" component={AbsenceForm} />
-              <Route path="/ListAbsence" component={Aabsence} />
-              <Route exact path="/AddChild" component={RegisterForm} />
-            </Switch>
+            {user ? (
+              <Switch>
+                <Route path="/ListChildren" component={ListChildren} />
+                <Route path="/Info" component={Info} />
+                <Route path="/Absence" component={AbsenceForm} />
+                <Route path="/ListAbsence" component={Aabsence} />
+                <Route exact path="/AddChild" component={RegisterForm} />
+              </Switch>
+            ) : (
+              <LoginForm onSubmit={validateUser} />
+            )}
           </div>
-
           <footer>
             <Footer />
           </footer>
         </div>
       </BrowserRouter>
-    );
-  }
+    </UserProvider>
+  );
 }
-
-export default App;
