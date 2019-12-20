a425c277c05620feafc7881b1c539a8744cde52f
cem2411 <cem.gueclue@hotmail.de>
2019-12-20 10:52:33 +0100

    login form

--- kiga2go-react/src/components/forms/login/LoginForm.js
+++ kiga2go-react/src/components/forms/login/LoginForm.js
@@ -1,87 +1,60 @@
-import React, { Component } from "react";
-import axios from "../../../services/GlobalAxiosSettings";
+import React, { useState } from "react";
 import logo from "../../../img/logo/logo.png";
 import "./style.scss";
 
-import { login } from "../../../services/userService";
+export default function LoginForm({ onSubmit }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export class LoginForm extends Component {
-  state = {
-    email: "",
-    password: ""
-  };
+  return (
+    <div className="login">
+      <form id="login__form">
+        <div className="login__header">
+          <span>Login</span>
+        </div>
 
-  componentDidMount() {
-    axios
-      .get("/rest/login")
-      .then(response => {
-        this.setState({
-          users: response.data
-        });
-
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  render() {
-    return (
-      <div className="login">
-        <form id="login__form">
-          <div className="login__header">
-            <span>Login</span>
+        <div className="loging__content">
+          <div className="login__content__image">
+            <img src={logo} href="kiga2go" height="150" />
           </div>
 
-          <div className="loging__content">
-            <div className="login__content__image">
-              <img src={logo} href="kiga2go" height="150" />
+          <div className="login__content__form">
+            <div className="login__content__form-group">
+              <label htmlFor="email">
+                <span>Email: </span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email eingeben..."
+                id="email"
+                onChange={event => setEmail(event.target.value)}
+              />
             </div>
 
-            <div className="login__content__form">
-              <div className="login__content__form-group">
-                <label htmlFor="username">
-                  <span>Email: </span>
-                </label>
-                <input
-                  type="email"
-                  name="username"
-                  placeholder="Email eingeben..."
-                  id="username"
-                  onChange={event => {
-                    this.setState({ email: event.target.value });
-                  }}
-                />
-              </div>
-
-              <div className="login__content__form-group">
-                <label htmlFor="password">
-                  <span>Passwort: </span>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Passwort eingeben..."
-                  id="password"
-                  onChange={event => {
-                    this.setState({ password: event.target.value });
-                  }}
-                />
-              </div>
+            <div className="login__content__form-group">
+              <label htmlFor="password">
+                <span>Passwort: </span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Passwort eingeben..."
+                id="password"
+                onChange={event => setPassword(event.target.value)}
+              />
             </div>
           </div>
-
-          <div className="login__footer"></div>
-        </form>
-        <button
-          className="login__footer__button"
-          id="button"
-          onClick={() => login(this.state.email, this.state.password)}
-        >
-          Login
-        </button>
-      </div>
-    );
-  }
+        </div>
+        <div className="login__footer"></div>
+      </form>
+      <button
+        className="login__footer__button"
+        id="button"
+        onClick={() => onSubmit({ email, password })}
+      >
+        Login
+      </button>
+    </div>
+  );
 }
